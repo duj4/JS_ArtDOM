25e0a185a0c7ece2bd0f750749a5dfc143fa2dce
duj4 <boyu.du@yahoo.com>
2019-12-11 15:20:48 +0800

    chapter 5

--- chapter_5/popup.js
+++ chapter_5/popup.js
@@ -4,6 +4,7 @@ window.onload = function() {
     if (!document.getElementsByTagName) {
         return false;
     }
+    
     // 这条语句在JS文件被加载时立刻执行
     // 如果JS文件从HTML的head通过外部引用的方式调用，它将在HTML文档加载之前加载到浏览器里
     // 如果JS文件位于HTML底部</body>之前，就无法保证哪个文件时最先结束加载，因为浏览器可以同时加载多个
