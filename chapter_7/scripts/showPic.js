671f09c5770839695aebc42579001e01435aec6e
duj4 <boyu.du@yahoo.com>
2019-12-16 22:36:13 +0800

    7.3

--- chapter_7/scripts/showPic.js
+++ chapter_7/scripts/showPic.js
@@ -1,19 +1,67 @@
+function addLoadEvent(func) {
+    // 把现有window.onload事件处理函数的值存入变量oldOnLoad
+    var oldOnLoad = window.onload;
+    // 如果在这个处理函数上还没有绑定任何函数，就像平时那样把新函数添加给它
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    }
+    // 如果在这个处理函数上已经绑定了一些函数，就把新函数追加到现有指令的末尾
+    else {
+        window.onload = function () {
+            oldOnLoad();
+            func();
+        }
+    }
+}
+
+// 手动实现JS中没有的insertAfter函数，将新元素插入到指定元素后面
+function insertAfter(newElement, targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement) {
+        parent.appendChild(newElement);
+    }
+    else {
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
+
+// placeholder和description只有在showPic时才会被使用，所以单独放到一个函数里，和HTML本身分开
 function preparePlaceholder() {
+    if (!document.createElement) {
+        return false;
+    }
+    if (!document.createTextNode) {
+        return false;
+    }
+    if (!document.getElementById) {
+        return false;
+    }
+    if (!document.getElementById("imagegallery")) {
+        return false
+    }
     var placeholder = document.createElement("img");
     placeholder.setAttribute("id", "placeholder");
     placeholder.setAttribute("src", "images/placeholder.gif");
     placeholder.setAttribute("alt", "my image gallery");
+
     var description = document.createElement("p");
     description.setAttribute("id", "description");
     var desctext = document.createTextNode("Choose an image");
     description.appendChild(desctext);
 
-    //把新创建的两个节点插入到body末尾，也可以使用document.getElementByTagName("body")[0].appendChild(placeholder)
-    document.body.appendChild(placeholder);
-    document.body.appendChild(description);
+    // 把新创建的两个节点插入到body末尾，也可以使用document.getElementByTagName("body")[0].appendChild(placeholder)
+    // 但只能把这两个元素加到body末尾，如果ul后面还有元素则没法保证原有顺序
+    // document.body.appendChild(placeholder);
+    // document.body.appendChild(description);
+
+    // 利用insertBefore可以在指定元素节点前面插入新元素
+    // 在gallery.html中，在ul的前面插入placeholder和description
+    var gallery = document.getElementById("imagegallery");
+    // gallery.parentNode.insertBefore(placeholder, gallery);
+    // gallery.parentNode.insertBefore(description, gallery);
+    insertAfter(placeholder, gallery);
+    insertAfter(description, placeholder);
 }
-
-
 
 function prepareGallery() {
     // 若当前浏览器不支持该方法，则直接退出
@@ -73,19 +121,5 @@ function showPic(whichpic) {
     return true;
 }
 
-function addLoadEvent(func) {
-    // 把现有window.onload事件处理函数的值存入变量oldOnLoad
-    var oldOnLoad = window.onload;
-    // 如果在这个处理函数上还没有绑定任何函数，就像平时那样把新函数添加给它
-    if (typeof window.onload != 'function') {
-        window.onload = func;
-    }
-    // 如果在这个处理函数上已经绑定了一些函数，就把新函数追加到现有指令的末尾
-    else {
-        window.onload = function () {
-            oldOnLoad();
-            func();
-        }
-    }
-}
+addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
